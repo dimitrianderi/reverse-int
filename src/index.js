6f58e041d03502269fa0b6566a00807fd0b2f7b5
Dmitry Kharitonov <dimitrianderi@gmail.com>
2022-01-09 01:57:29 +0500

    feat: add content to function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    let number = Math.abs(n).toString();
+    let new_number = '';
+
+    for (let i = number.length-1; i >= 0; i--) {
+        let letter = number[i];
+        new_number += letter;
+    }
+
+    return +new_number;
+
 }
